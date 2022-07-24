805e6a76481323b39b10fb762a5c3a5d8c5a5d38
SNChanger <skuazt81@hotmail.co.jp>
2022-07-24 14:56:06 +0900

    データベースアプリケーション拡張
    　・キーボード入力受付
    　・接続先のAPIサーバー情報を取得

--- Program.cs
+++ Program.cs
@@ -3,11 +3,11 @@ using System.Net.Http;
 using MediaDataBaseModelShare;
 using System.Text.Json;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("作品を収集するよ");
+Console.ForegroundColor = ConsoleColor.Gray;
 
-// メディアデータベース
-Console.WriteLine("メディアDBの分類一覧だよ");
+HttpClient client = new HttpClient();
+HttpResponseMessage response;
+
 
 Dictionary<MediaType, string> mediaTypePairs = new Dictionary<MediaType, string>();
 mediaTypePairs.Add(MediaType.Manga, "manga");
@@ -16,55 +16,113 @@ mediaTypePairs.Add(MediaType.Game, "game");
 mediaTypePairs.Add(MediaType.MediaArt, "mediaart");
 mediaTypePairs.Add(MediaType.Collection, "collection");
 
-HttpClient client = new HttpClient();
-HttpResponseMessage response;
 
-List<MediaRecordRow> mediaRecordRows = new List<MediaRecordRow>();
+Console.WriteLine("サーバーの状態を取得します");
 
 
-foreach (KeyValuePair<MediaType, string> pair in mediaTypePairs) {
-    Console.WriteLine($"{pair.Value}のカテゴリー一覧を取得する");
-    Console.WriteLine($"https://mediaarts-db.bunka.go.jp/api/field?fieldId={pair.Value}");
-    response = await client.GetAsync($"https://mediaarts-db.bunka.go.jp/api/field?fieldId={pair.Value}");
-    response.EnsureSuccessStatusCode();
-    string fieldResponseBody = await response.Content.ReadAsStringAsync();
+response = await client.GetAsync($"https://mediaarts-db.bunka.go.jp/api/openapi.json");
+response.EnsureSuccessStatusCode();
+if (response.StatusCode != System.Net.HttpStatusCode.OK) {
+    Console.WriteLine("openapiへの通信に失敗したので、検索システムを終了します");
+    return;
+}
+string openApiResponseBody = await response.Content.ReadAsStringAsync();
 
-    FieldResponse fieldResponse = JsonSerializer.Deserialize<FieldResponse>(fieldResponseBody);
+OpenApiResponse openApiResponse = JsonSerializer.Deserialize<OpenApiResponse>(openApiResponseBody);
 
-   foreach (var fieldRow in fieldResponse.Fields) {
-        foreach (var category in fieldRow.FieldCategories) {
-            foreach (var subCategory in category.SubCategories)
+while (true)
+{
+    Console.WriteLine("実行したいコマンドを入力してね");
+    Console.WriteLine("1:メディア情報検索");
+    Console.WriteLine("2:バージョン情報");
+    Console.WriteLine("3:Apiサーバー情報");
+    Console.WriteLine("4:終了");
+    var readKey = Console.ReadKey();
+    switch (readKey.Key)
+    {
+        case ConsoleKey.NumPad1:
+            SearchMediaDatabase(client);
+            break;
+        case ConsoleKey.NumPad2:
+            Console.WriteLine("version 0.1");
+            break;
+        case ConsoleKey.NumPad3:
+            Console.WriteLine("メディア芸術データベース　利用元情報");
+            Console.WriteLine($"オープンapi バージョン {openApiResponse.OpenApiVersion}");
+            break;
+        case ConsoleKey.NumPad4:
+            Console.WriteLine("アプリを終了するよ");
+            return;
+    }
+    Console.WriteLine("メニューに戻る場合は、キーを押してね");
+    readKey = Console.ReadKey();
+    Console.Clear();
+
+}
+
+
+
+
+
+
+
+async void SearchMediaDatabase(HttpClient client){
+    // See https://aka.ms/new-console-template for more information
+    Console.WriteLine("作品を収集するよ");
+
+    // メディアデータベース
+    Console.WriteLine("メディアDBの分類一覧だよ");
+
+    List<MediaRecordRow> mediaRecordRows = new List<MediaRecordRow>();
+
+
+    foreach (KeyValuePair<MediaType, string> pair in mediaTypePairs)
+    {
+        Console.WriteLine($"{pair.Value}のカテゴリー一覧を取得する");
+        response = await client.GetAsync($"https://mediaarts-db.bunka.go.jp/api/field?fieldId={pair.Value}");
+        response.EnsureSuccessStatusCode();
+        string fieldResponseBody = await response.Content.ReadAsStringAsync();
+
+        FieldResponse fieldResponse = JsonSerializer.Deserialize<FieldResponse>(fieldResponseBody);
+
+        foreach (var fieldRow in fieldResponse.Fields)
+        {
+            foreach (var category in fieldRow.FieldCategories)
             {
-                var mediaRecordRow = new MediaRecordRow();
-                mediaRecordRow.FieldId = pair.Value;
-                mediaRecordRow.CategoryId = category.CategoryId;
-                mediaRecordRow.SubCategoryId = subCategory.SubCategoryId;
-                mediaRecordRows.Add(mediaRecordRow);
+                foreach (var subCategory in category.SubCategories)
+                {
+                    var mediaRecordRow = new MediaRecordRow();
+                    mediaRecordRow.FieldId = pair.Value;
+                    mediaRecordRow.CategoryId = category.CategoryId;
+                    mediaRecordRow.SubCategoryId = subCategory.SubCategoryId;
+                    mediaRecordRows.Add(mediaRecordRow);
+                }
             }
         }
+
+    }
+
+
+    foreach (MediaRecordRow mediaRecord in mediaRecordRows)
+    {
+        Console.WriteLine($"{mediaRecord.FieldId}_{mediaRecord.CategoryId}_{mediaRecord.SubCategoryId}の情報を収集するよ");
+        client = new HttpClient();
+        response = await client.GetAsync($"https://mediaarts-db.bunka.go.jp/api/search?fieldId={mediaRecord.FieldId}&categoryId={mediaRecord.CategoryId}&subcategoryId={mediaRecord.SubCategoryId}&sort=date&limit=1000");
+        // 分類の新規予約による作品制作中の場合は収集しない
+        if (response.StatusCode != System.Net.HttpStatusCode.OK)
+        {
+            continue;
+        }
+        response.EnsureSuccessStatusCode();
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        SearchResponse searchResponse = JsonSerializer.Deserialize<SearchResponse>(responseBody);
+        DisplaySearchResponse(searchResponse);
+
     }
 
 }
-
-
-foreach (MediaRecordRow mediaRecord in mediaRecordRows) {
-    Console.WriteLine($"{mediaRecord.FieldId}_{mediaRecord.CategoryId}_{mediaRecord.SubCategoryId}の情報を収集するよ");
-    client = new HttpClient();
-    response = await client.GetAsync($"https://mediaarts-db.bunka.go.jp/api/search?fieldId={mediaRecord.FieldId}&categoryId={mediaRecord.CategoryId}&subcategoryId={mediaRecord.SubCategoryId}&sort=date&limit=1000");
-    // 分類の新規予約による作品制作中の場合は収集しない
-    if (response.StatusCode != System.Net.HttpStatusCode.OK) {
-        continue;
-    }
-    response.EnsureSuccessStatusCode();
-
-    string responseBody = await response.Content.ReadAsStringAsync();
-
-    SearchResponse searchResponse = JsonSerializer.Deserialize<SearchResponse>(responseBody);
-    DisplaySearchResponse(searchResponse);
-
-}
-
-
 
 void DisplaySearchResponse(SearchResponse searchResponse) {
 
